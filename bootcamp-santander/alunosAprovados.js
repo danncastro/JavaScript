f02868ef5f5d0672da5ef1f7af223f07e862357f
danniel.castro <dann-castro@hotmail.com>
2022-06-24 20:44:23 -0300

    docs

---
+++ bootcamp-santander/alunosAprovados.js
@@ -0,0 +1,7 @@
+/*
+1. Crie uma função que recebe o array 'alunos' e um número que irá representar a média final
+2. Percorra o array e popule um novo array auxiliar apenas com os alunos cujas notas são 
+   maiores ou iguais á média final;
+3. Utilize a técnica "objetect destructuring" para manipular as propriedades deseadas
+   de cada aluno
+*/
